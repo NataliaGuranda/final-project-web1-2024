5bffb20468486e9c28860c475d0e20005f39729a
NataliaGuranda <112927127+NataliaGuranda@users.noreply.github.com>
2024-03-11 21:28:40 +0200

    Add files via upload

---
+++ Produse.jsx
@@ -0,0 +1,196 @@
+import React from "react";
+import i1 from './img/i1.png';
+import i11 from './img/i11.jpg';
+import i12 from './img/i12.jpg';
+import i13 from './img/i13.jpg';
+import i14 from './img/i14.jpg';
+import i15 from './img/i15.jpg';
+import i16 from './img/i16.jpg';
+import i17 from './img/i17.jpg';
+import i18 from './img/i18.jpg';
+import i19 from './img/i19.jpg';
+import i20 from './img/i20.jpg';
+import i21 from './img/i21.jpg';
+import i23 from './img/i23.jpg';
+import a1 from './img/a1.jpg';
+import a2 from './img/a2.jpg';
+import a3 from './img/a3.jpg';
+import a4 from './img/a4.jpg';
+import a5 from './img/a5.jpg';
+import n1 from './img/n1.jpg';
+import n2 from './img/n2.jpg';
+import n3 from './img/n3.jpg';
+import n4 from './img/n4.jpg';
+
+
+function Produse(){
+return(
+<>
+
+<div class="produse1">Produse de Post</div> <br/>
+<section class="img_shop_1">
+    <div class="img_shop">
+        <img src={i11}/>
+        <div class="product_info">
+            <span class="product_name">Produs 1</span>
+            <span class="product_price">100 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop">
+    <img src={i12}/>
+        <div class="product_info">
+            <span class="product_name">Produs 2</span>
+            <span class="product_price">100 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop">
+    <img src={i13}/>
+        <div class="product_info">
+            <span class="product_name">Produs 3</span>
+            <span class="product_price">100 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop">
+    <img src={i14}/>
+        <div class="product_info">
+            <span class="product_name">Produs 4</span>
+            <span class="product_price">100 Lei</span>
+        </div>
+    </div>
+</section>
+
+<section class="img_shop_2">
+    <div class="img_shop">
+    <img src={i15}/>
+        <div class="product_info">
+            <span class="product_name">Produs 5</span>
+            <span class="product_price">100 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop">
+    <img src={i16}/>
+        <div class="product_info">
+            <span class="product_name">Produs 6</span>
+            <span class="product_price">100 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop">
+    <img src={i17}/>
+        <div class="product_info">
+            <span class="product_name">Produs 7</span>
+            <span class="product_price">100 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop">
+    <img src={i18}/>
+        <div class="product_info">
+            <span class="product_name">Produs 8</span>
+            <span class="product_price">100 Lei</span>
+        </div>
+    </div>
+</section>
+
+<section class="img_shop_3">
+    <div class="img_shop">
+    <img src={i19}/>
+        <div class="product_info">
+            <span class="product_name">Produs 9</span>
+            <span class="product_price">100 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop">
+    <img src={i20}/>
+        <div class="product_info">
+            <span class="product_name">Produs 10</span>
+            <span class="product_price">100 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop">
+    <img src={i21}/>
+        <div class="product_info">
+            <span class="product_name">Produs 11</span>
+            <span class="product_price">100 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop">
+    <img src={i23}/>
+        <div class="product_info">
+            <span class="product_name">Produs 12</span>
+            <span class="product_price">100 Lei</span>
+        </div>
+    </div>
+</section>
+<div class="produse1">Pentru Aniversări</div>
+<section class="img_shop_4">
+    <div class="img_shop_a">
+    <img src={a1} height="270" width="270px"/>
+        <div class="product_info">
+            <span class="product_name">Produs Aniversare 1</span>
+            <span class="product_price">150 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop_a">
+    <img src={a2} height="270" width="270px"/>
+        <div class="product_info">
+            <span class="product_name">Produs Aniversare 2</span>
+            <span class="product_price">200 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop_a">
+    <img src={a3} height="270" width="270px"/>
+        <div class="product_info">
+            <span class="product_name">Produs Aniversare 3</span>
+            <span class="product_price">180 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop_a">
+    <img src={a4} height="270" width="270px"/>
+        <div class="product_info">
+            <span class="product_name">Produs Aniversare 4</span>
+            <span class="product_price">220 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop_a">
+    <img src={a5} height="270" width="270px"/>
+        <div class="product_info">
+            <span class="product_name">Produs Aniversare 5</span>
+            <span class="product_price">190 Lei</span>
+        </div>
+    </div>
+</section>
+
+<div class="produse1">Pentru Nuntă</div>
+<section class="img_shop_5">
+    <div class="img_shop_a">
+    <img src={n1} height="270" width="270px"/>
+        <div class="product_info">
+            <span class="product_name">Produs Nuntă 1</span>
+            <span class="product_price">300 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop_a">
+    <img src={n2} height="270" width="270px"/>
+        <div class="product_info">
+            <span class="product_name">Produs Nuntă 2</span>
+            <span class="product_price">250 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop_a">
+    <img src={n3} height="270" width="270px"/>
+        <div class="product_info">
+            <span class="product_name">Produs Nuntă 3</span>
+            <span class="product_price">280 Lei</span>
+        </div>
+    </div>
+    <div class="img_shop_a">
+    <img src={n4} height="270" width="270px"/>
+        <div class="product_info">
+            <span class="product_name">Produs Nuntă 4</span>
+            <span class="product_price">320 Lei</span>
+        </div>
+    </div>
+</section>
+</>
+)
+}
+export default Produse;
